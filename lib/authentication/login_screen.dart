b78c7a1b16a31df8a14254664089aa47f7fe3660
balayogi789 <balyogigmcs@gmail.com>
2024-07-09 16:21:25 -0400

    all files

---
+++ lib/authentication/login_screen.dart
@@ -0,0 +1,151 @@
+import 'package:cccc/authentication/signup_screen.dart';
+// import 'package:cccc/methods/common_methods.dart';
+import 'package:cccc/pages/homepage.dart';
+// // import 'package:cccc/widgets/loading_dialog.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// //import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController usernameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  // CommonMethods cmethods = CommonMethods();
+
+  // checkIfNetworkAvailable(){
+  //   cmethods.checkConnectivity(context);
+
+  //   signInFormValidation();
+  // }
+
+  // signInFormValidation(){
+  //   if(emailTextEditingController.text.trim().length < 4){
+  //     cmethods.displaySnackbar('Your Username must be atleast 4 characters', context);
+  //   }
+  //   else if(passwordTextEditingController.text.trim().length < 6){
+  //     cmethods.displaySnackbar('Your Password must be atleast 6 characters', context);
+  //   }
+  //   else{
+  //     signInForm();
+  //   }
+  // }
+
+  // signInForm() async{
+  //   showDialog(
+  //       context: context,
+  //       builder: (BuildContext context) =>
+  //           LoadingDialog(messageText: " Allowing user to Login"));
+
+  //   final User? userFirebase = (await FirebaseAuth.instance
+  //           .signInWithEmailAndPassword(
+  //               email: emailTextEditingController.text.trim(),
+  //               password: passwordTextEditingController.text.trim())
+  //           .catchError((errorMsg) {
+  //     Navigator.pop(context);
+  //     cmethods.displaySnackbar(errorMsg.toString(), context);
+  //   }))
+  //       .user;
+  //   if (!context.mounted) return;
+  //   Navigator.pop(context);
+
+  //   if(userFirebase != null){
+  //     DatabaseReference usersRef = FirebaseDatabase.instance.ref().child('users').child(userFirebase.uid);
+  //     usersRef.once().then((snap){
+  //       if(snap.snapshot.value != null){
+  //         Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Homepage()));
+  //       }
+  //       else{
+  //         cmethods.displaySnackbar('Account doesn\'t exist', context);
+  //       }
+  //     });
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.asset("assets/images/logo.png"),
+              const Text(
+                'Login As a User',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                          labelText: ' email',
+                          labelStyle: TextStyle(fontSize: 14)),
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          labelText: ' password',
+                          labelStyle: TextStyle(fontSize: 14)),
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // checkIfNetworkAvailable();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Homepage()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 80, vertical: 10)),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignupScreen()));
+                      },
+                      child: const Text(
+                        'Don\'t have a account? Register here',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,67 @@
+// import 'dart:async';
+// import 'dart:convert';
+// import 'dart:typed_data';
+
+// import 'package:cccc/global/global_var.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:geolocator_android/geolocator_android.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  // final Completer<GoogleMapController> googleMapCompleterController = Completer<GoogleMapController>();
+  // GoogleMapController? controllerGoogleMap;
+  // Position? currentPositionOfUser;
+
+  // void updateMapTheme(GoogleMapController controller) {
+  //   getJsonFileFromThemes('themes/dark_style.json').then((value) => setGoogleMapStyle(value, controller));
+  // }
+
+  // Future<String> getJsonFileFromThemes(String path) async {
+  //   ByteData byteData = await rootBundle.load(path);
+  //   var list = byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+  //   return utf8.decode(list);
+  // }
+
+  // void setGoogleMapStyle(String googleMapStyle, GoogleMapController controller) {
+  //   controller.setMapStyle(googleMapStyle);
+  // }
+
+  // getCurrentLiveLocationOfUser() async{
+  //   Position positionOfUser = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
+  //   currentPositionOfUser = positionOfUser;
+
+  //   LatLng positionOfUserInLatLng = LatLng(currentPositionOfUser!.latitude,currentPositionOfUser!.longitude);
+  //   CameraPosition cameraPosition = CameraPosition(target: positionOfUserInLatLng,zoom: 15);
+  //   controllerGoogleMap!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Home page')),
+      // body: Stack(
+      //   children: [
+      //     GoogleMap(
+      //       mapType: MapType.normal,
+      //       myLocationButtonEnabled: true,
+      //       initialCameraPosition: googlePlexInitialPositon,
+      //       onMapCreated: (GoogleMapController mapController) {
+      //         controllerGoogleMap = mapController;
+      //         updateMapTheme(controllerGoogleMap!);
+      //         googleMapCompleterController.complete(controllerGoogleMap);
+      //       },
+      //     ),
+      //   ],
+      // ),
+    );
+  }
+}
